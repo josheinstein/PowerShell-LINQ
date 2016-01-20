745425a1c3dc26b30306a899f51a0423d03d9410
Josh Einstein <josh@josheinstein.com>
2016-01-20 18:33:04 -0500

    Ew, I used tab indent?

--- Tests/Arrays.tests.ps1
+++ Tests/Arrays.tests.ps1
@@ -4,217 +4,217 @@ $Numbers = @(1..10)
 
 Describe 'Array.Any' {
 
-	It 'returns $true' {
-		$Numbers.Any({$_ -eq 1}) | Should Be $true
-	}
+    It 'returns $true' {
+        $Numbers.Any({$_ -eq 1}) | Should Be $true
+    }
 
-	It 'returns $false' {
-		$Numbers.Any({$_ -eq 0}) | Should Be $false
-	}
+    It 'returns $false' {
+        $Numbers.Any({$_ -eq 0}) | Should Be $false
+    }
 
 }
 
 Describe 'Array.All' {
 
-	It 'returns $true' {
-		$Numbers.All({$_ -gt 0}) | Should Be $true
-	}
+    It 'returns $true' {
+        $Numbers.All({$_ -gt 0}) | Should Be $true
+    }
 
-	It 'returns $false' {
-		$Numbers.All({$_ -lt 10}) | Should Be $false
-	}
+    It 'returns $false' {
+        $Numbers.All({$_ -lt 10}) | Should Be $false
+    }
 
 }
 
 Describe 'Array.CountWhere' {
 
-	It 'counts odd numbers' {
-		$Numbers.CountWhere({$_ % 2}) | Should Be 5
-	}
+    It 'counts odd numbers' {
+        $Numbers.CountWhere({$_ % 2}) | Should Be 5
+    }
 
-	It 'counts without predicate' {
-		$Numbers.CountWhere() | Should Be 10
-	}
+    It 'counts without predicate' {
+        $Numbers.CountWhere() | Should Be 10
+    }
 
 }
 
 Describe 'Array.First' {
 
-	It 'gets first item' {
-		$Numbers.First() | Should Be 1
-	}
+    It 'gets first item' {
+        $Numbers.First() | Should Be 1
+    }
 
-	It 'gets first item matching predicate' {
-		$Numbers.First({$_ -gt 5}) | Should Be 6
-	}
+    It 'gets first item matching predicate' {
+        $Numbers.First({$_ -gt 5}) | Should Be 6
+    }
 
 }
 
 Describe 'Array.Last' {
 
-	It 'gets last item' {
-		$Numbers.Last() | Should Be 10
-	}
+    It 'gets last item' {
+        $Numbers.Last() | Should Be 10
+    }
 
-	It 'gets last item matching predicate' {
-		$Numbers.Last({$_ -lt 5}) | Should Be 4
-	}
+    It 'gets last item matching predicate' {
+        $Numbers.Last({$_ -lt 5}) | Should Be 4
+    }
 
 }
 
 Describe 'Array.FindIndex' {
 
-	It 'gets index by value' {
-		$Numbers.FindIndex(5) | Should Be 4
-	}
+    It 'gets index by value' {
+        $Numbers.FindIndex(5) | Should Be 4
+    }
 
-	It 'gets index by predicate' {
-		$Numbers.FindIndex({$_ -eq 5}) | Should Be 4
-	}
+    It 'gets index by predicate' {
+        $Numbers.FindIndex({$_ -eq 5}) | Should Be 4
+    }
 
-	It 'cannot find value' {
-		$Numbers.FindIndex(0) | Should Be -1
-	}
+    It 'cannot find value' {
+        $Numbers.FindIndex(0) | Should Be -1
+    }
 
-	It 'cannot find predicate' {
-		$Numbers.FindIndex({$_ -eq 0}) | Should Be -1
-	}
+    It 'cannot find predicate' {
+        $Numbers.FindIndex({$_ -eq 0}) | Should Be -1
+    }
 
 }
 
 Describe 'Array.Single' {
 
-	It 'gets single item matching predicate' {
-		$Numbers.Single({$_ -eq 1}) | Should Be 1
-	}
+    It 'gets single item matching predicate' {
+        $Numbers.Single({$_ -eq 1}) | Should Be 1
+    }
 
-	It 'gets null on empty array' {
-		@().Single() | Should Be $Null
-	}
+    It 'gets null on empty array' {
+        @().Single() | Should Be $Null
+    }
 
-	It 'throws on multiple results' {
-		{ $Numbers.Single() } | Should Throw
-		{ $Numbers.Single({$_ -lt 3}) } | Should Throw
-	}
+    It 'throws on multiple results' {
+        { $Numbers.Single() } | Should Throw
+        { $Numbers.Single({$_ -lt 3}) } | Should Throw
+    }
 
 }
 
 Describe 'Array.Skip' {
 
-	It 'skips some items' {
-		$Numbers.Skip(5) | Should Be @(6..10)
-	}
+    It 'skips some items' {
+        $Numbers.Skip(5) | Should Be @(6..10)
+    }
 
-	It 'skips all items' {
-		$Numbers.Skip(10) | Should Be $Null
-	}
+    It 'skips all items' {
+        $Numbers.Skip(10) | Should Be $Null
+    }
 
-	It 'skips no items' {
-		$Numbers.Skip(0) | Should Be $Numbers
-	}
+    It 'skips no items' {
+        $Numbers.Skip(0) | Should Be $Numbers
+    }
 
 }
 
 Describe 'Array.Take' {
 
-	It 'takes some items' {
-		$Numbers.Take(5) | Should Be @(1..5)
-	}
+    It 'takes some items' {
+        $Numbers.Take(5) | Should Be @(1..5)
+    }
 
-	It 'takes all items' {
-		$Numbers.Take(10) | Should Be $Numbers
-	}
+    It 'takes all items' {
+        $Numbers.Take(10) | Should Be $Numbers
+    }
 
-	It 'takes no items' {
-		$Numbers.Take(0) | Should Be $Null
-	}
+    It 'takes no items' {
+        $Numbers.Take(0) | Should Be $Null
+    }
 
 }
 
 Describe 'Array.SkipWhile' {
 
-	It 'skips some items' {
-		$Numbers.SkipWhile({$_ -le 5}) | Should Be @(6..10)
-	}
+    It 'skips some items' {
+        $Numbers.SkipWhile({$_ -le 5}) | Should Be @(6..10)
+    }
 
-	It 'skips all items' {
-		$Numbers.SkipWhile({$_ -le 10}) | Should Be $Null
-	}
+    It 'skips all items' {
+        $Numbers.SkipWhile({$_ -le 10}) | Should Be $Null
+    }
 
-	It 'skips no items' {
-		$Numbers.SkipWhile({$_ -gt 10}) | Should Be $Numbers
-	}
+    It 'skips no items' {
+        $Numbers.SkipWhile({$_ -gt 10}) | Should Be $Numbers
+    }
 
 }
 
 Describe 'Array.TakeWhile' {
 
-	It 'takes some items' {
-		$Numbers.TakeWhile({$_ -le 5}) | Should Be @(1..5)
-	}
+    It 'takes some items' {
+        $Numbers.TakeWhile({$_ -le 5}) | Should Be @(1..5)
+    }
 
-	It 'takes all items' {
-		$Numbers.TakeWhile({$_ -le 10}) | Should Be $Numbers
-	}
+    It 'takes all items' {
+        $Numbers.TakeWhile({$_ -le 10}) | Should Be $Numbers
+    }
 
-	It 'takes no items' {
-		$Numbers.TakeWhile({$_ -gt 10}) | Should Be $Null
-	}
+    It 'takes no items' {
+        $Numbers.TakeWhile({$_ -gt 10}) | Should Be $Null
+    }
 
 }
 
 Describe 'Array.Average' {
 
-	It 'averages numbers' {
-		$Numbers.Average() | Should Be 5.5
-	}
+    It 'averages numbers' {
+        $Numbers.Average() | Should Be 5.5
+    }
 
-	It 'averages numbers with selector' {
-		$Numbers.Average({$_ * 2}) | Should Be 11
-	}
+    It 'averages numbers with selector' {
+        $Numbers.Average({$_ * 2}) | Should Be 11
+    }
 
 }
 
 Describe 'Array.Sum' {
 
-	It 'sums numbers' {
-		$Numbers.Sum() | Should Be 55
-	}
+    It 'sums numbers' {
+        $Numbers.Sum() | Should Be 55
+    }
 
-	It 'sums numbers with selector' {
-		$Numbers.Sum({$_ * 2}) | Should Be 110
-	}
+    It 'sums numbers with selector' {
+        $Numbers.Sum({$_ * 2}) | Should Be 110
+    }
 
 }
 
 Describe 'Array.Min' {
 
-	It 'gets min value' {
-		$Numbers.Min() | Should Be 1
-	}
+    It 'gets min value' {
+        $Numbers.Min() | Should Be 1
+    }
 
-	It 'gets min value with selector' {
-		$Numbers.Min({$_ * 2}) | Should Be 2
-	}
+    It 'gets min value with selector' {
+        $Numbers.Min({$_ * 2}) | Should Be 2
+    }
 
-	It 'gets null from empty set' {
-		@().Min() | Should Be $Null
-	}
+    It 'gets null from empty set' {
+        @().Min() | Should Be $Null
+    }
 
 }
 
 Describe 'Array.Max' {
 
-	It 'gets max value' {
-		$Numbers.Max() | Should Be 10
-	}
+    It 'gets max value' {
+        $Numbers.Max() | Should Be 10
+    }
 
-	It 'gets max value with selector' {
-		$Numbers.Max({$_ * 2}) | Should Be 20
-	}
+    It 'gets max value with selector' {
+        $Numbers.Max({$_ * 2}) | Should Be 20
+    }
 
-	It 'gets null from empty set' {
-		@().Max() | Should Be $Null
-	}
+    It 'gets null from empty set' {
+        @().Max() | Should Be $Null
+    }
 
 }
--- Tests/ConvertTo-Dictionary.tests.ps1
+++ Tests/ConvertTo-Dictionary.tests.ps1
@@ -2,62 +2,62 @@
 
 Describe "ConvertTo-Dictionary" {
 
-	Context "Hashtables" {
+    Context "Hashtables" {
 
-		BeforeEach {
+        BeforeEach {
 
-			$Objects = @(
-				[PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1962-02-06"}
-				[PSCustomObject]@{Name='Slash'; DOB=[DateTime]"1965-07-23"}
-				[PSCustomObject]@{Name='Duff'; DOB=[DateTime]"1964-02-05"}
-				[PSCustomObject]@{Name='Izzy'; DOB=[DateTime]"1962-04-08"}
-				[PSCustomObject]@{Name='Steven'; DOB=[DateTime]"1965-01-22"}
-			)
+            $Objects = @(
+                [PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1962-02-06"}
+                [PSCustomObject]@{Name='Slash'; DOB=[DateTime]"1965-07-23"}
+                [PSCustomObject]@{Name='Duff'; DOB=[DateTime]"1964-02-05"}
+                [PSCustomObject]@{Name='Izzy'; DOB=[DateTime]"1962-04-08"}
+                [PSCustomObject]@{Name='Steven'; DOB=[DateTime]"1965-01-22"}
+            )
 
-		}
+        }
 
-		It "creates a Hashtable" {
-			$Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}
-			{ $Dictionary -is [Hashtable] } | Should Be $true
-		}
+        It "creates a Hashtable" {
+            $Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}
+            { $Dictionary -is [Hashtable] } | Should Be $true
+        }
 
-		It "has key/object pair" {
-			$Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}
-			$Dictionary.Axl | Should Not Be $Null
-			$Dictionary.Axl.DOB | Should Be ([DateTime]"1962-02-06")
-		}
+        It "has key/object pair" {
+            $Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}
+            $Dictionary.Axl | Should Not Be $Null
+            $Dictionary.Axl.DOB | Should Be ([DateTime]"1962-02-06")
+        }
 
-		It "has key/value pair" {
-			$Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}	{$_.DOB}
-			$Dictionary.Axl | Should Not Be $Null
-			$Dictionary.Axl | Should Be ([DateTime]"1962-02-06")
-		}
+        It "has key/value pair" {
+            $Dictionary = $Objects | ConvertTo-Dictionary {$_.Name}	{$_.DOB}
+            $Dictionary.Axl | Should Not Be $Null
+            $Dictionary.Axl | Should Be ([DateTime]"1962-02-06")
+        }
 
-	}
+    }
 
-	Context "Duplicate Keys" {
+    Context "Duplicate Keys" {
 
-		BeforeEach {
+        BeforeEach {
 
-			$Objects = @(
-				[PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1962-02-06"}
-				[PSCustomObject]@{Name='Slash'; DOB=[DateTime]"1965-07-23"}
-				[PSCustomObject]@{Name='Duff'; DOB=[DateTime]"1964-02-05"}
-				[PSCustomObject]@{Name='Izzy'; DOB=[DateTime]"1962-04-08"}
-				[PSCustomObject]@{Name='Steven'; DOB=[DateTime]"1965-01-22"}
-				[PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1900-01-01"}
-			)
+            $Objects = @(
+                [PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1962-02-06"}
+                [PSCustomObject]@{Name='Slash'; DOB=[DateTime]"1965-07-23"}
+                [PSCustomObject]@{Name='Duff'; DOB=[DateTime]"1964-02-05"}
+                [PSCustomObject]@{Name='Izzy'; DOB=[DateTime]"1962-04-08"}
+                [PSCustomObject]@{Name='Steven'; DOB=[DateTime]"1965-01-22"}
+                [PSCustomObject]@{Name='Axl'; DOB=[DateTime]"1900-01-01"}
+            )
 
-		}
+        }
 
-		It "throws without -force" {
-			{ $Objects | ConvertTo-Dictionary {$_.Name} } | Should Throw
-		}
+        It "throws without -force" {
+            { $Objects | ConvertTo-Dictionary {$_.Name} } | Should Throw
+        }
 
-		It "doesn't throw on duplicate key with -force" {
-			{ $Objects | ConvertTo-Dictionary {$_.Name} -Force } | Should Not Throw
-		}
+        It "doesn't throw on duplicate key with -force" {
+            { $Objects | ConvertTo-Dictionary {$_.Name} -Force } | Should Not Throw
+        }
 
-	}
+    }
 
 }
